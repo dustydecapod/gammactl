3808d3378ae981c9cfb4c271f1595852451dcba5
Ber Zoidberg <ber.zoidberg@gmail.com>
2021-08-18 00:12:21 -0700

    add pygobject to reqs; add as-percent option for brightness; fix unintentional output

--- gammactl.py
+++ gammactl.py
@@ -11,6 +11,7 @@ temperaturectl = svc_proxy['net.zoidplex.wlr_gamma_service.temperature']
 
 _terse = False
 _quiet = False
+_as_percent = False
 
 @click.group()
 @click.option('--quiet', default=False, is_flag=True)
@@ -21,39 +22,48 @@ def cli(quiet, terse):
     _quiet = quiet
 
 @cli.group()
-def brightness():
-    pass
+@click.option('--as-percent', default=False, is_flag=True)
+def brightness(as_percent):
+    global _as_percent
+    _as_percent = as_percent
 
 @brightness.command()
 def get():
     rval = round(brightnessctl.get(), 2)
+    if _as_percent: rval = round(rval * 100)
     if _quiet: return
-    if _terse: print(rval)
-    print("Current brightness: %.2f (%i%%)" % (rval, rval*100))
+    elif _terse: print(rval)
+    else: print("Current brightness: %.2f" % rval)
 
 @brightness.command()
 @click.argument('value', default=1.0)
 def set(value):
+    if _as_percent: value /= 100.0
     rval = brightnessctl.set(value)
+    if _as_percent: rval = round(rval * 100)
     if _quiet: return
-    if _terse: print(rval)
-    print("Current brightness: %.2f (%i%%)" % (rval, rval*100))
+    elif _terse: print(rval)
+    else: print("Current brightness: %.2f" % rval)
 
 @brightness.command()
 @click.argument('amount', default=0.05)
 def increase(amount):
+    if _as_percent: amount /= 100
     rval = brightnessctl.increase(amount)
+    if _as_percent: rval = round(rval * 100)
     if _quiet: return
-    if _terse: print(rval)
-    print("Current brightness: %.2f (%i%%)" % (rval, rval*100))
+    elif _terse: print(rval)
+    else: print("Current brightness: %.2f" % rval)
 
 @brightness.command()
 @click.argument('amount', default=0.05)
 def decrease(amount):
+    if _as_percent: amount /= 100
     rval = brightnessctl.decrease(amount)
+    if _as_percent: rval = round(rval * 100)
     if _quiet: return
-    if _terse: print(rval)
-    print("Current brightness: %.2f (%i%%)" % (rval, rval*100))
+    elif _terse: print(rval)
+    else: print("Current brightness: %.2f" % rval)
 
 @cli.group()
 def temperature():
@@ -63,30 +73,30 @@ def temperature():
 def get():
     rval = temperaturectl.get()
     if _quiet: return
-    if _terse: print(rval)
-    print("Current temperature: %iK" % rval)
+    elif _terse: print(rval)
+    else: print("Current temperature: %iK" % rval)
 
 @temperature.command()
 @click.argument('value', default=6500)
 def set(value):
     rval = temperaturectl.set(value)
     if _quiet: return
-    if _terse: print(rval)
-    print("Current temperature: %iK" % rval)
+    elif _terse: print(rval)
+    else: print("Current temperature: %iK" % rval)
 
 @temperature.command()
 @click.argument('amount', default=100)
 def increase(amount):
     rval = temperaturectl.increase(amount)
     if _quiet: return
-    if _terse: print(rval)
-    print("Current temperature: %iK" % rval)
+    elif _terse: print(rval)
+    else: print("Current temperature: %iK" % rval)
 
 @temperature.command()
 @click.argument('amount', default=100)
 def decrease(amount):
     rval = temperaturectl.decrease(amount)
     if _quiet: return
-    if _terse: print(rval)
-    print("Current temperature: %iK" % rval)
+    elif _terse: print(rval)
+    else: print("Current temperature: %iK" % rval)
 
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     install_requires=[
         'Click',
 		'pydbus',
-        'pygobject'
+        'PyGObject'
     ],
     entry_points={
         'console_scripts': [
